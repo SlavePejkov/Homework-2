c1bb598ecb87fec7bd715c7ce9032d07c4cdee96
SlavePejkov <119795961+SlavePejkov@users.noreply.github.com>
2022-12-08 18:03:27 +0100

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,49 @@
+let theYear = prompt("Hey what year are you born in?");
+console.log("The user is born in: ", theYear);
+
+let chineseZodiac = (theYear - 4) % 12;
+console.log(chineseZodiac);
+
+
+
+if (chineseZodiac === 0) {
+    console.log("The user's Chinese Zodiac sign is a Rat")
+} 
+else if(chineseZodiac === 1) {
+    console.log("The user's Chinese Zodiac sign is a Ox")
+} 
+else if(chineseZodiac === 2) {
+    console.log("The user's Chinese Zodiac sign is a Tiger")
+}
+else if (chineseZodiac === 3) {
+    console.log("The user's Chinese Zodiac sign is a Rabbit")
+}
+else if (chineseZodiac === 4) {
+    console.log("The user's Chinese Zodiac sign is a Dragon")
+}
+else if (chineseZodiac === 5) {
+    console.log("The user's Chinese Zodiac sign is a Snake")
+}
+else if (chineseZodiac === 6) {
+    console.log("The user's Chinese Zodiac sign is a Horse")
+}
+else if (chineseZodiac === 7) {
+    console.log("The user's Chinese Zodiac sign is a Goat")
+}
+else if (chineseZodiac === 8) {
+    console.log("The user's Chinese Zodiac sign is a Monkey")
+}
+else if (chineseZodiac === 9) {
+    console.log("The user's Chinese Zodiac sign is a Rooster")
+}
+else if (chineseZodiac === 10) {
+    console.log("The user's Chinese Zodiac sign is a Dod")
+}
+else if (chineseZodiac === 11) {
+    console.log("The user's Chinese Zodiac sign is a Pig")
+}
+
+
+
+
+
